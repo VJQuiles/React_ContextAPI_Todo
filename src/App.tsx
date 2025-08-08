d1959c1a58d9436bfd74df33785010ca52a2b604
Vince Quiles <vjquiles@msn.com>
2025-08-08 13:55:01 -0400

    Got the toggle theme to work. I think it's conflicting with the settings on my pc, instead of the whole page going between light and dark, only a box in the middle is though now that I think of it, it may be the doc fragment throwing me off.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import './App.css'
 import TodoInput from './components/TodoInput/TodoInput'
 import TodoItem from './components/TodoItem/TodoItem'
 import TodoList from './components/TodoList/TodoList'
+import { useContext } from 'react'
+import { ThemeContext } from './contexts/contexts'
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
   return (
     <>
-      <TodoList />
-      <TodoInput />
-      <TodoItem />
+      <div
+        style={{
+          backgroundColor: theme === 'dark' ? "#121212" : "#fff",
+          color: theme === 'dark' ? "#fff" : '#000'
+        }}>
+        <button
+          onClick={toggleTheme}>Clap On/Off</button>
+        <TodoList />
+        <TodoInput />
+        <TodoItem />
+      </div>
     </>
   )
 }
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,15 +1,8 @@
-import { useContext } from "react"
-import { ThemeContext } from "../../contexts/contexts"
-//import { ThemeContextType } from "../../types"
-
 export default function TodoList() {
-    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return (
-        <div
-            style={{ color: theme === 'dark' ? 'white' : 'black' }}
-        >
+        <div>
             Todo List
-            <button onClick={toggleTheme}>Clap On/Off</button>
         </div>
     )
 }
