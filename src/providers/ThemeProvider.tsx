b3e719431d2e0f0335a3429a4ec773509f46790a
Vince Quiles <vjquiles@msn.com>
2025-08-08 19:47:44 -0400

    Added localStorage, and core functionality is complete. This definitely flipped my view of components. Readme underway

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,18 +1,26 @@
-import { useState, type ReactNode, useMemo } from "react"
+import { useState, type ReactNode, useEffect } from "react"
 import { ThemeContext } from "../contexts/contexts"
-import { type ThemeContextType } from "../types"
 
 interface TodoProviderProps {
     children: ReactNode
 }
 
 export const ThemeProvider = ({ children }: TodoProviderProps) => {
-    const [theme, setTheme] = useState<ThemeContextType['theme']>('light')
-    const toggleTheme = () => setTheme((t) => (t === 'light' ? 'dark' : 'light'))
-    const themeValue: ThemeContextType = useMemo(() => ({ theme, toggleTheme }), [theme])
+    const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+        const storedTheme = localStorage.getItem('theme')
+        return storedTheme === 'dark' ? 'dark' : 'light'
+    })
+
+    const toggleTheme = () => {
+        setTheme((t) => (t === 'light' ? 'dark' : 'light'))
+    }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+    }, [theme])
 
     return (
-        <ThemeContext.Provider value={themeValue}>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
         </ThemeContext.Provider>
     )
